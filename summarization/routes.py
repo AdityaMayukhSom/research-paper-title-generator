d8cae3dd231d4cfc48234b4a15329d5fa5dcc6d9
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-07-29 01:19:48 +0530

    [ADD]: route to return available models

--- summarization/routes.py
+++ summarization/routes.py
@@ -7,3 +7,17 @@ import os
 
 
 api_router = fastapi.APIRouter(prefix="/api")
+
+
+@api_router.get("/available-models")
+def available_models():
+    available_models_list: list[str] = [
+        "TohidaRehman/pegasus-large-Abstract-Title-CSPubSum",
+        "TohidaRehman/t5-base-Abstract-Title",
+        "TohidaRehman/bart-base-Abstract-Title-CSPubSum",
+        "czearing/article-title-generator",
+        "AryanLala/autonlp-Scientific_Title_Generator-34558227",
+        "Fabby/gpt2-english-light-novel-titles",
+    ]
+
+    return {"models": available_models_list}
