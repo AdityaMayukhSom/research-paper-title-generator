52ae2c2033574912d7d658795a47db6553ec4f9a
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-07-29 01:22:02 +0530

    [ADD]: function to generate application

--- summarization/application.py
+++ summarization/application.py
@@ -12,3 +12,19 @@ class SummarizationApplication:
     __allowed_headers: list[str] = ["*"]
     __allowed_methods: list[str] = ["*"]
     __allowed_credentials: bool = True
+
+    @classmethod
+    def generate(cls):
+        app = fastapi.FastAPI()
+
+        app.add_middleware(
+            fastapi.middleware.cors.CORSMiddleware,
+            allow_origins=cls.__allowed_origins,
+            allow_methods=cls.__allowed_methods,
+            allow_headers=cls.__allowed_headers,
+            allow_credentials=cls.__allowed_credentials,
+        )
+
+        app.include_router(api_router)
+
+        return app
